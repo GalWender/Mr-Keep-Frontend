d5895d7ba28f1170aec8c26801ab891091c6100d
Gal Wender <galwender@gmail.com>
2023-05-22 22:30:48 +0300

    fixed autofocus feature of in the input of adding

--- src/app/cmps/note-add/note-add.component.ts
+++ src/app/cmps/note-add/note-add.component.ts
@@ -1,5 +1,5 @@
 import { UtilService } from './../../services/util.service';
-import { Component, OnInit} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'note-add',
@@ -7,25 +7,34 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./note-add.component.scss']
 })
 export class NoteAddComponent implements OnInit {
+  @ViewChild("addTitle") addTitle?: ElementRef
+  @ViewChild("addText") addText?: ElementRef
   constructor(
-  ) {}
+  ) { }
 
   addNoteTextToggle: boolean = false
 
   ngOnInit(): void {
-
+    setTimeout(() => {
+      this.addText?.nativeElement.focus()
+    }, 100)
   }
 
   onOpenAddNoteText() {
     this.addNoteTextToggle = true
+    setTimeout(() => {
+      this.addText?.nativeElement.focus()
+    }, 100)
   }
-  
+
   onCloseAddNoteText() {
-    this.addNoteTextToggle = false    
+    this.addNoteTextToggle = false
   }
 
   clickedOutside(): void {
     this.addNoteTextToggle = false;
+    console.log('title:', this.addTitle?.nativeElement.value);
+    console.log('text:', this.addText?.nativeElement.value);
   }
 
 }
